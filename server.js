a00bc74d6ce01730089210dfcc60f8f2c01f40c9
Mykola Kolotylo <mykolakolotilo@gmail.com>
2020-07-02 08:52:13 +0300

    add methods to server js file

--- server.js
+++ server.js
@@ -7,4 +7,27 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json( {success: true, msg: 'Show all bootcamps'} )
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    const { params: { id } } = req;
+    res.status(200).json( {success: true, msg: `Get bootcamp ${id}`} );
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json( {success: true, msg: 'Create new bootcamp'} )
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    const { params: { id } } = req;
+    res.status(200).json( {success: true, msg: `Update bootcamp ${id}`} );
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    const { params: { id } } = req;
+    res.status(200).json( {success: true, msg: `Delete bootcamp ${id}`} );
+});
+
 app.listen(PORT, () => `App is running on ${PORT} port`);
